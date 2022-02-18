4dbefa006119f04634394d0905f69195fcf0f800
jon-whiteroomsoftware <59668898+jon-whiteroomsoftware@users.noreply.github.com>
2022-02-18 11:10:05 -0700

    Added unmounter to test fetch aborting

--- src/index.js
+++ src/index.js
@@ -3,9 +3,29 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import CatApp from "./cats/CatApp";
 
+function Unmounter({ children }) {
+  const [isMounted, setIsMounted] = React.useState(true);
+
+  function onChange(e) {
+    setIsMounted(!isMounted);
+  }
+
+  return (
+    <>
+      <div>
+        <label>Mounted</label>
+        <input type="checkbox" onChange={onChange} checked={isMounted} />
+      </div>
+      <div>{isMounted && children}</div>
+    </>
+  );
+}
+
 ReactDOM.render(
   <React.StrictMode>
+    {/* <Unmounter> */}
     <CatApp />
+    {/* </Unmounter> */}
   </React.StrictMode>,
   document.getElementById("root")
 );
